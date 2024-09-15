b6c28887cd77aefb27e80b2c31fff9c66bd3a1ad
Olena-Ishchenko <elena.dubovalova@gmail.com>
2024-09-16 01:12:29 +0300

    add swiper

---
+++ src/js/gallery.js
@@ -0,0 +1,42 @@
+import Swiper from 'swiper';
+import {
+  Navigation,
+  Scrollbar,
+  EffectCoverflow,
+  Keyboard,
+  Mousewheel,
+} from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
+const swiperParams = {
+  modules: [Navigation, Scrollbar, EffectCoverflow, Keyboard, Mousewheel],
+
+  breakpoints: {
+    320: { slidesPerView: 2, spaceBetween: 16 },
+
+    1440: {
+      slidesPerView: 3,
+      spaceBetween: 32,
+    },
+  },
+
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  pagination: {
+    el: '.swiper-pagination',
+    // clickable: true,
+  },
+
+  keyboard: {
+    enabled: true,
+  },
+
+  mousewheel: true,
+};
+
+const swiper = new Swiper('.swiper', swiperParams);
